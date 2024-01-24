90851a3bd458d65a7b4fe35ef75bcdccadf5685e
simon.lecordier <simon.lecordier@fortil.group>
2024-01-24 15:06:31 +0100

    Revert "Runnable project & dockerfile"
    This reverts commit 12c24b8d

--- docker/mysql-server/build.bat
+++ docker/mysql-server/build.bat
@@ -1,7 +1,2 @@
-docker container prune
-docker image prune
-
 docker build . -t mysql
-docker run -it -p 33060:3306 -d --name mysql1 mysql
-
-docker exec -it mysql1 mysql -uroot -p
+docker run -it mysql
