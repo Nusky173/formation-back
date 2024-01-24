4a1ec955109ad68e53a07af3a73a9a2301f1aea0
simon.lecordier <simon.lecordier@fortil.group>
2024-01-24 15:07:37 +0100

    update bat script

--- docker/mysql-server/build.bat
+++ docker/mysql-server/build.bat
@@ -1,2 +1,7 @@
+docker container prune
+docker image prune
+
 docker build . -t mysql
-docker run -it mysql
+docker run -it -p 33060:3306 -d --name mysql1 mysql
+
+docker exec -it mysql1 mysql -uroot -p
